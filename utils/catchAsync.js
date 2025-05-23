314fe137413d6ceb6ac385f6a01cfefaa2393a81
Rushi Donga <rushi.donga.04@gmail.com>
2025-05-23 09:47:43 -0400

    Updated CatchAsync

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,5 @@
 exports.catchAsync = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(err => (errnext));
-    }
-}
+  return (req, res, next) => {
+    fn(req, res, next).catch(err => next(err));
+  };
+};
